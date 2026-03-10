3181648628ddba649e3c88fee79af4b3843afb19
Saito-Dai <dai2525yuu@icloud.com>
2026-03-10 15:26:47 +0900

    common.pyファイル完成

---
+++ app/schemas/common.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel, Field
+# エンドポイントで呼びだして、ドキュメントに反映させよう。また、エラーを返すときに使う。
+class HealthResponse(BaseModel): # ヘルスチェック用のレスポンス
+    status: str = Field(..., examples=["ok"]) # ...は必須であることを、examplesはstatusに入る値のサンプル。今回は正常とわかる簡易的なものでよし。
+    
+class ErrorResponse(BaseModel): # 共通のエラーレスポンス
+    detail: str = Field(..., examples=["Repository not found"])
